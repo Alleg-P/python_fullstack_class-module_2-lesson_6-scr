91ab881f34e8acc16b040ad7094d7dfc9038fea4
Alleg <opankov@inbox.ru>
2023-12-24 20:15:08 +0300

    fixed

--- solution_8.py
+++ solution_8.py
@@ -1,4 +1,5 @@
 # Задание 8: Перестановка товаров
 
-produсts = "Чай", "Мёд", "Сахар"
-print(f"Товары на полке:", produсts[::-1])
+produсts = ["Чай", "Мёд", "Сахар"]
+produсts[0], produсts[2] = produсts[2], produсts[0]
+print(f"Товары на полке:", produсts)
